222ade4b7918eb05279d19f114047acf9a318b1a
ideal1sm <xddanil48@gmail.com>
2023-08-21 14:26:41 +0300

    add spam checker;

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -6,6 +6,7 @@ use App\Entity\Comment;
 use App\Entity\Conference;
 use App\Form\CommentType;
 use App\Repository\CommentRepository;
+use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
@@ -30,7 +31,8 @@ class ConferenceController extends AbstractController
         Request                           $request,
         Conference                        $conference,
         CommentRepository                 $commentRepository,
-        #[Autowire('%photo_dir%')] string $photoDir
+        #[Autowire('%photo_dir%')] string $photoDir,
+        SpamChecker $spamChecker,
 
     ): Response
     {
@@ -47,6 +49,17 @@ class ConferenceController extends AbstractController
             }
 
             $this->entityManager->persist($comment);
+
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+            ];
+            if (2 === $spamChecker->getSpamScore($comment, $context)) {
+                throw new \RuntimeException('Spam, go away!');
+            }
+
             $this->entityManager->flush();
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
